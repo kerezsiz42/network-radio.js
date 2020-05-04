ef9a582f316b687f8e5fad27414b999a3136e6af
unknown <kerezsiz42@gmail.com>
2020-05-04 14:42:25 +0200

    theme-color, urls updated

--- public/script.js
+++ public/script.js
@@ -8,10 +8,10 @@ function playAudio() {
     } else {
         buttonPressed = true;
     }
-    fetch('http://wirebirds.club/getCurrent', {method: "POST"})
+    fetch('https://radio.wirebirds.club/getCurrent', {method: "POST"})
     .then(response => response.json())
     .then(randomMusic => {
-        source.src = `http://wirebirds.club/music/${randomMusic}`;
+        source.src = `https://radio.wirebirds.club/music/${randomMusic}`;
         audio.load();
         audio.addEventListener('ended', listener = () => {
             playAudio();
